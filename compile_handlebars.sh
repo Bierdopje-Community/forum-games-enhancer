ca3769c664b6ec1d7ff2b03351dd06482806cfa2
Robin Houtevelts <robin@houtevelts.com>
2016-01-24 17:31:05 +0100

    Separate script and tampermonkey-data Remove compiled handlebars files

--- compile_handlebars.sh
+++
@@ -1,8 +0,0 @@
-command -v handlebars >/dev/null 2>&1 || { echo >&2 "handlebars not found. Please install using npm install -g handlebars"; exit 1; }
-
-OUTPUTFILE="dist/html.templates.js";
-
-mkdir -p dist 2> /dev/null;
-truncate -s 0 "$OUTPUTFILE"; #empty file
-
-find ./html -name "*.hbs" -exec handlebars -e "hbs" -m "{}" >> "$OUTPUTFILE" \;
